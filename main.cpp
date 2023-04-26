4ef851e8cd72d3ca36a5f6b1edbd3ed533869602
jkusnierz01 <jedrek33@onet.pl>
2023-04-26 22:05:03 +0200

    almost quicksort

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main()
     int n = 8;
     int tab[8] = {14,1,2,5,4,9,6,6};
     int tab2[8] = {12,5,2,1,4,3,6,6};
+    int tab3[8] = {10,5,8,1,4,3,9,6};
     merge_sort(tab,n);
     for(int z =0;z<n;z++)
     {
@@ -18,6 +19,12 @@ int main()
     for(int z =0;z<n;z++)
     {
        cout << tab2[z] << " "; 
-    }  
+    } 
+    cout<<endl;
+    quicksort(tab3, n);  
+    for(int z =0;z<n;z++)
+    {
+       cout << tab3[z] << " "; 
+    } 
     return 0;
 }
--- quicksort.hh
+++ quicksort.hh
@@ -3,10 +3,38 @@
 #include <iostream>
 using namespace std;
 
-
-
-
-
-
+void quicksort(int tab[], int tab_size, int left)
+{
+    if (tab_size > 1)
+    {
+        int right;
+        int tmp;
+        int pivot = tab_size / 2;
+        tmp = tab[tab_size - 1];
+        tab[tab_size - 1] = tab[pivot];
+        tab[pivot] = tmp;
+        while (left < right)
+        {
+            while (tab[right] > tab[tab_size - 1])
+            {
+                right--;
+            }
+            while (tab[left] < tab[tab_size - 1])
+            {
+                left++;
+            }
+            tmp = tab[left];
+            tab[left] = tab[right];
+            tab[right] = tmp;
+            right--;
+            left++;
+        }
+        tmp = tab[tab_size - 1];
+        tab[tab_size - 1] = tab[pivot];
+        tab[pivot] = tmp;
+        quicksort(tab, pivot , 0);
+        quicksort(tab, tab_size - pivot - 1, pivot);
+    }
+}
 
 #endif
